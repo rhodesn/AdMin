2d7ab104d2d26643a23cbf55fbe7587d718be86b
Nick Rhodes <nrhodes91@gmail.com>
2018-11-24 06:52:05 +0000

    Correct shellcheck warnings in the parse-sass-sh scripts

--- gnome-shell/parse-sass.sh
+++ gnome-shell/parse-sass.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-if [ ! "$(which sassc 2> /dev/null)" ]; then
+if [ ! "$(command -v sassc &>/dev/null)" ]; then
    echo sassc needs to be installed to generate the css.
    exit 1
 fi
 
-SASSC_OPT="-l -M -t expanded"
+SASSC_OPT=("-l" "-M" "-t" "e xpanded")
 
 echo Generating the css...
 
-sassc $SASSC_OPT gnome-shell.scss gnome-shell.css
+sassc "${SASSC_OPT[@]}" gnome-shell.scss gnome-shell.css
