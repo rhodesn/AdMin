b666a18c3d3aa064bdf8723bf744dbe7bf72733f
Nick Rhodes <nrhodes91@gmail.com>
2019-04-07 07:56:18 +0100

    Add new OSX button choice

---
+++ admin-gtk3-dark-osx/gtk-3.0/parse-sass.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+if ! command -v sassc &>/dev/null; then
+   echo sassc needs to be installed to generate the css.
+   exit 1
+fi
+
+SASSC_OPT=("-l" "-M" "-t" "expanded")
+
+echo Generating the css...
+
+sassc "${SASSC_OPT[@]}" gtk-contained.scss gtk-contained.css
+sassc "${SASSC_OPT[@]}" gtk-contained-dark.scss gtk-contained-dark.css
+
+ln -sf "gtk-${1:-dark}.css" gtk.css
