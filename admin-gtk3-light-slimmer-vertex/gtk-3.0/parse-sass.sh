2080a01392037ffb89ab3bbc08eecf4168d6a35b
Nick Rhodes <nrhodes91@gmail.com>
2019-06-27 13:16:37 +0100

    Add new Vertex slimmer variant

---
+++ admin-gtk3-light-slimmer-vertex/gtk-3.0/parse-sass.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+if ! command -v sassc &>/dev/null; then
+   echo sassc needs to be installed to generate the css.
+   exit 1
+fi
+
+SASSC_OPT=("-l" "-M" "-t" "expanded")
+
+echo Generating the css...
+
+sassc "${SASSC_OPT[@]}" gtk-contained.scss gtk.css
+sassc "${SASSC_OPT[@]}" gtk-contained-dark.scss gtk-dark.css
