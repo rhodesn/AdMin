586e4ef281b912b73d088a7aec2d81af115b87aa
Nick Rhodes <nrhodes91@gmail.com>
2020-02-10 20:35:47 +0000

    Update Dash to Dock styling in gnome-shell

--- admin-gtk3-light/gnome-shell/parse-sass.sh
+++ admin-gtk3-light/gnome-shell/parse-sass.sh
@@ -5,8 +5,10 @@ if ! command -v sassc &>/dev/null; then
    exit 1
 fi
 
+gsettings set org.gnome.shell.extensions.user-theme name ''
 #SASSC_OPT=('-t' 'compressed')
 SASSC_OPT=('-t' 'compact')
 echo Generating the css...
 
 sassc "${SASSC_OPT[@]}" gnome-shell.scss gnome-shell.css
+gsettings set org.gnome.shell.extensions.user-theme name 'admin-gtk3-light'
